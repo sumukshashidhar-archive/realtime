95e4526def830db265c5c820400537b1a737e8b8
sumukshashidhar <sumukuuu@gmail.com>
2021-06-15 07:56:05 +0000

    compression

---
+++ compress.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import os
+import logging
+
+
+logging.debug("Running Compression")
+
+for filename in os.listdir('./prices/'):
+    if filename.endswith('.csv'):
+        try:
+            fil = os.path.join('./prices/', filename)
+            df = pd.read_csv(fil, header=None)
+            df.drop_duplicates(subset=[1, 2, 3, 4, 5, 6, 7], keep='last')
+            df.to_csv(fil, header=False, index=False)
+        except Exception as e:
+            logging.error(f"Failed to compress file {filename}. {e}")
+
+
+
+for filename in os.listdir('./depth/'):
+    if filename.endswith('.csv'):
+        try:
+            fil = os.path.join('./depth/', filename)
+            df = pd.read_csv(fil, header=None)
+            df.drop_duplicates(subset=[1, 2, 3, 4], keep='last')
+            df.to_csv(fil, header=False, index=False)
+        except Exception as e:
+            logging.error(f"Failed to compress file {filename}. {e}")
